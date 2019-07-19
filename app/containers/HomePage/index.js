b05bcf184f043f68e6da167a6545a9dedb364702
svetlana berrios <sveta.berrios@gmail.com>
2019-07-19 13:08:05 +0300

    set up firebase

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -8,11 +8,28 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import { Component } from 'react';
+import logo from '../../images/logo.jpg';
+
+export class Header extends Component {
+  render () {
+    return (
+      <div className="row">
+        <div className="logo">
+          <img src={logo} width="100" height="50" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default function HomePage() {
   return (
     <h1>
+      <Header/>
       <FormattedMessage {...messages.header} />
     </h1>
   );
 }
+
+
