01e07d3410cdd0422f3f1d78c5e97294bf4b0e9b
svetlana berrios <sveta.berrios@gmail.com>
2019-07-19 14:23:03 +0300

    added some libs, and styled components

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,6 +1,5 @@
 /*
  * HomePage
- *
  * This is the first thing users see of our App, at the '/' route
  *
  */
@@ -11,19 +10,66 @@ import messages from './messages';
 import { Component } from 'react';
 import logo from '../../images/logo.jpg';
 
+
+const App = () => (
+  <Grid.Provider
+    padding="20px"
+    breakpoints={{ sm: "-500", md: "501-750", lg: "+750" }}
+  >
+    <Grid.Bounds direction="vertical">
+      <Grid.Box sm={{ hidden: true }}>
+        This header hides on small screens
+      </Grid.Box>
+      <Grid.Box>Content</Grid.Box>
+      <Grid.Box lg={{ padding: "50px" }}>
+        This footer gains extra padding on large screens
+      </Grid.Box>
+    </Grid.Bounds>
+  </Grid.Provider>
+);
+
+
+
 export class Header extends Component {
   render () {
     return (
-      <div className="row">
         <div className="logo">
-          <img src={logo} width="100" height="50" />
+          <img src={logo} width="500" height="800" />
         </div>
-      </div>
     );
   }
 }
 
+export class Sidebar extends Component {
+  render () {
+    return (
+      <div className="row">
+
+      </div>
+    )
+  }
+}
+
 export default function HomePage() {
+  const content=[
+      {
+          icon: 'icon-class-name',
+          label: 'Label of Item',
+          to: '#a-link',
+      },
+      {
+          icon: 'icon-class-name',
+          label: 'Second Item',
+          content: [
+              {
+                  icon: 'icon-class-name',
+                  label: 'Sub Menu of Second Item',
+                  to: '#another-link',
+              },
+          ],
+      },
+  ];
+
   return (
     <h1>
       <Header/>
@@ -32,4 +78,5 @@ export default function HomePage() {
   );
 }
 
+//ReactDOM.render(<MetisMenu />, document.getElementById('dom_id'));
 
