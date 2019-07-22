9d50513e344caa0a56abf51bdabcb708508106b3
svetlana berrios <sveta.berrios@gmail.com>
2019-07-22 12:23:34 +0300

    demo

---
+++ Airtable.js
@@ -0,0 +1,6 @@
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyBCQBasy6oWdP63'
+});
+var base = Airtable.base('app3H9oIfok6KQvG5');
